398216c96c55932ecbd75dac9c69bd0fc5e6b271
liming <voidee@139.com>
2025-02-13 15:43:10 +0800

    add scripts for installing conda and zsh

---
+++ scripts/var.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+
+RED="\e[31m"
+GREEN="\e[32m"
+BLUE="\e[34m"
+RESET="\e[0m"
---
+++ scripts/zsh.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -e
+source ./var.sh
+# 安装 zsh
+if ! which zsh &> /dev/null
+then
+    echo -e "${RED}Zsh is not installed, installing...${RESET}"
+    sudo apt-get install -y zsh
+else
+    echo -e "${RED}Zsh is already installed.${RESET}"
+fi
+
+# 安装 oh-my-zsh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo -e "${RED}Installing Oh My Zsh...${RESET}"
+    wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O - | sh
+else
+    echo -e "${RED}Oh My Zsh is already installed.${RESET}"
+fi
+
+# 克隆 zsh-autosuggestions 插件
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
+    echo -e "${RED}Cloning zsh-autosuggestions plugin...${RESET}"
+    git clone https://githuab.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+else
+    echo -e "${RED}zsh-autosuggestions plugin is already installed.${RESET}"
+fi
+
+# 克隆 zsh-syntax-highlighting 插件
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
+    echo -e "${RED}Cloning zsh-syntax-highlighting plugin...${RESET}"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+else
+    echo -e "${RED}zsh-syntax-highlighting plugin is already installed.${RESET}"
+fi
+
+# 激活插件
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' $HOME/.zshrc
